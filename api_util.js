5bdea2485e146b00afcad06f19eb601858cb8701
Fernando <mhfernando778@gmail.com>
2025-03-30 15:25:50 -0400

    Moved code from util to main

--- api_util.js
+++ api_util.js
@@ -1,35 +1,35 @@
-//Task 3 - Fetch Products with async/await //
+// //Task 3 - Fetch Products with async/await //
 
-import { fetchProductsThen } from "./api.js";
+// import { fetchProductsThen } from "./api.js";
 
-async function fetchProductsAsync() {
-    try {
-      const res = await fetch(BASE_URL);
-      if (!response.ok) {
-        throw new Error(`HTTP ERROR: ${response.status}`);
-      }
-      return await res.json();
-      displayProducts(products);
-    } catch (error) {
-      handleError(error);
-    }
-  }
+// async function fetchProductsAsync() {
+//     try {
+//       const res = await fetch(BASE_URL);
+//       if (!response.ok) {
+//         throw new Error(`HTTP ERROR: ${response.status}`);
+//       }
+//       return await res.json();
+//       displayProducts(products);
+//     } catch (error) {
+//       handleError(error);
+//     }
+//   }
 
 
 
-// Task 4 - Display the Product //
+// // Task 4 - Display the Product //
 
-function displayProducts(products) {
-    const container = document.getElementById('product-container');
-    container.innerHTML = '';
-    products.slice(0, 5).forEach(product => {
-      const productCard = document.createElement('div');
-      productCard.classList.add('product');
-      productCard.innerHTML = `
-        <img src="${product.fields.image[0].url}" alt="${product.fields.name}" />
-        <h2>${product.fields.name}</h2>
-        <p>Price: $${product.fields.price}</p>
-      `;
-      container.appendChild(productCard);
-    });
-  }
+// function displayProducts(products) {
+//     const container = document.getElementById('product-container');
+//     container.innerHTML = '';
+//     products.slice(0, 5).forEach(product => {
+//       const productCard = document.createElement('div');
+//       productCard.classList.add('product');
+//       productCard.innerHTML = `
+//         <img src="${product.fields.image[0].url}" alt="${product.fields.name}" />
+//         <h2>${product.fields.name}</h2>
+//         <p>Price: $${product.fields.price}</p>
+//       `;
+//       container.appendChild(productCard);
+//     });
+//   }
